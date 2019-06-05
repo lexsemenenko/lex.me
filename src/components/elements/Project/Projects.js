0deda8e404adedd83549bfd5ac9d44a2ee6c52e4
Lex Semenenko <lex@lex.me>
2019-06-05 04:57:23 -0400

    Added scroll component to projects modal content

--- src/components/elements/Project/Projects.js
+++ src/components/elements/Project/Projects.js
@@ -55,7 +55,6 @@ const Projects = () => {
                 style={styleToggle}
                 id={id}
                 onClick={e => {
-                  console.log(e.currentTarget)
                   onToggleOpen(e)
                 }}
               >
@@ -71,15 +70,160 @@ const Projects = () => {
                       isActive ? 'open' : 'closed'
                     }`}
                   >
-                    <div>{company}</div>
-                    <button
-                      type="button"
-                      onClick={e => {
-                        onToggleCloseAll(e)
-                      }}
-                    >
-                      Close
-                    </button>
+                    <div className="scroll-projects">
+                      <div className="scroll-projects__header">
+                        <h3>{name}</h3>
+                        <p>{company}</p>
+
+                        <button
+                          className="project__close"
+                          type="button"
+                          onClick={e => {
+                            onToggleCloseAll(e)
+                          }}
+                        >
+                          Close
+                        </button>
+                      </div>
+                      <div className="scroll-projects__body">
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                        <p>
+                          Pellentesque habitant morbi tristique senectus et
+                          netus et malesuada fames ac turpis egestas. Vestibulum
+                          tortor quam, feugiat vitae, ultricies eget, tempor sit
+                          amet, ante. Donec eu libero sit amet quam egestas
+                          semper. Aenean ultricies mi vitae est. Mauris placerat
+                          eleifend leo. Quisque sit amet est et sapien
+                          ullamcorper pharetra. Vestibulum erat wisi,
+                          condimentum sed, commodo vitae, ornare sit amet, wisi.
+                          Aenean fermentum, elit eget tincidunt condimentum,
+                          eros ipsum rutrum orci, sagittis tempus lacus enim ac
+                          dui. Donec non enim in turpis pulvinar facilisis. Ut
+                          felis. Praesent dapibus, neque id cursus faucibus,
+                          tortor neque egestas augue, eu vulputate magna eros eu
+                          erat. Aliquam erat volutpat. Nam dui mi, tincidunt
+                          quis, accumsan porttitor, facilisis luctus, metus
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
