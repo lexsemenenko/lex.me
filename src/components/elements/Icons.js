08345ee568533d3a3b4839137290ee487ab67ad4
Lex Semenenko <lex@lex.me>
2019-07-03 15:07:21 -0400

    Logo styles

--- src/components/elements/Icons.js
+++ src/components/elements/Icons.js
@@ -75,6 +75,7 @@ const Icon = ({name, size, className, fill, ...attrs}) => {
       case 'logo':
         return (
           <path
+            fill="currentColor"
             d="M39 151h60v146h74V151h162l26 42 28-42h67l-60 96 66 102h-72l-31-48-29 48H39zm195 146h59l32-49-28-45h-64l33 45z"
             paintOrder="stroke"
           />
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -9,7 +9,17 @@ const Header = () => (
   <header id="header" className="section header scrollpoint-header">
     <div className="header__logo">
       <a href="/">
-        <Image src="/images/lex-logo.svg" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="39 151 423 198"
+          aria-hidden="true"
+        >
+          <path
+            fill="currentColor"
+            d="M39 151h60v146h74V151h162l26 42 28-42h67l-60 96 66 102h-72l-31-48-29 48H39zm195 146h59l32-49-28-45h-64l33 45z"
+            paintOrder="stroke"
+          />
+        </svg>
       </a>
     </div>
     <Dropdown blockClass="menu-mobile">
