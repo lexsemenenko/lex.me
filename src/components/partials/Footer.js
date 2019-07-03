6f7bc07b029c94aed71777e53e7dbe77f824c5f5
Lex Semenenko <lex@lex.me>
2019-07-03 14:46:06 -0400

    Mew footer styles

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -4,6 +4,7 @@ import Icon from '../elements/Icons';
 const Footer = () => (
   <footer id="footer" className="section footer">
     <Icon name="logo" size="4em" className="footer__logo" />
+    <p className="t-italic">Created with React</p>
   </footer>
 );
 
