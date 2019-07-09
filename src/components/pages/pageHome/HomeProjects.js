0b1a1cf8bb88d2a55f7b62a1a3f2073bedf9b2c9
Lex Semenenko <lex@lex.me>
2019-07-08 20:00:10 -0400

    Projects lead text updated

--- src/components/pages/pageHome/HomeProjects.js
+++ src/components/pages/pageHome/HomeProjects.js
@@ -7,6 +7,13 @@ const HomeProjects = () => {
       <h2 className="section__title">
         <span>Projects</span>
       </h2>
+      <div data-grid="columns: 12">
+        <div data-grid-item="width: 8, offset: 2, width-large: 6, offset-large: 3">
+          <p className="t-lead t-center">
+            Below, you can see some projects I&apos;ve been working on lately.
+          </p>
+        </div>
+      </div>
       <StoreContentful />
     </div>
   );
